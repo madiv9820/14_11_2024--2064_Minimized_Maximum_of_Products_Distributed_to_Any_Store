54c4cb8275158605d97f3f60859e0053f76dd11a
madiv9820 <themathur901@gmail.com>
2024-11-14 23:51:29 +0530

    C++ Solution implemented using Linear Search Approach

--- Solution.cpp
+++ Solution.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 class Solution {
 public:
-    int minimizedMaximum(int n, vector<int> quantities) {
+    int minimizedMaximum(int n, vector<int>& quantities) {
         
         // Lambda function to check if it's possible to distribute the quantities into `n` stores,
         // where no store can have more than `x` items.
